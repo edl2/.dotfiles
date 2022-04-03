8f3421a08420b4990d06eaafebb773caef6af840
edl2 <ellew010@gmail.com>
2022-04-03 11:27:09 -0400

    fixed bashrc

--- dwm/config.h
+++ dwm/config.h
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *browsercmd[]  = { "vieb", NULL };
+static const char *browsercmd[]  = { "chromium", NULL };
 static const char *discordcmd[] = { "discord", NULL };
 static const char *filescmd[] = { "ranger", NULL };
 static Key keys[] = {
