42c6cbb288461f39051214aa854b412964e3394b
edl2 <ellew010@gmail.com>
2022-04-27 19:04:47 -0400

    misc. changes

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,6 @@
+c.url.searchengines = {'DEFAULT': 'https://www.startpage.com/sp/search?query={}'}
+config.load_autoconfig()
+c.tabs.tooltips = False
+c.window.hide_decoration = True
+
+
--- dwm/config.h
+++ dwm/config.h
@@ -12,9 +12,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#282a36";
+static const char *fonts[]          = { "monospace:size=11", "monospace:size=11" };
+static const char dmenufont[]       = "monospace:size=11";
+static const char col_gray1[]       = "#383c4a";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5" };
+static const char *tags[] = { "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
